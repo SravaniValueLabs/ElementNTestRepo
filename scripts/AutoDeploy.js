b13f20af013a53d19a671143a8f3e0cdcb01500c
SravaniValueLabs <sravanivaluelabs@gmail.com>
2015-12-14 12:07:46 +0545

    4

---
+++ scripts/AutoDeploy.js
@@ -0,0 +1,10 @@
+/** Script ACLs do not delete 
+ read=nobody 
+write=nobody
+execute=authenticated 
+  **/ 
+ //This is a test file and commit this to test auto-deploy
+return "Hello 123.Test Done"
+
+//Performing changes in file;
+			
